a286eb5bed6cf1fc4121aca300091afc3ecc1f03
Chik011 <chikokasa11@gmail.com>
2025-05-31 21:45:09 +0700

    1

--- plugins/toram.js
+++ plugins/toram.js
@@ -579,7 +579,32 @@ Legendary Ornament: 450-500m`, m)
       return;
     }
     if (command == 'lvlgbs') {
-      await conn.reply(m.chat, `Harga belum pasti sesuai, bisa berubah`, m)
+      await conn.reply(m.chat, `LEVELING BS
+
+Versi BS VIT
+▫0-15 Baju Pengelana
+▫15-120 Baju Diomedia
+▫120-140 Tombak Baskara
+▫140-170 Katan Bakung Lelabah Merah
+▫170-200 2H Golok Pembasmi Naga / 1H Pedang Archanida
+▫200-225 Baju Dara / Kostum Pengelabu
+▫225-250 2H Pholidata / Kostum Pengelabu
+▫250-300 Gak Tau
+
+|----------------------------------------------------------|
+
+
+Versi BS Tombak/Tinju,MD/Staff
+▫0-15 Baju Pengelana
+▫15-50 Tinju Hard Knucles
+▫50-90 Pedang Indigo
+▫90-120 Baju Diomedia
+▫120-140 Tombak Baskara
+▫140-170 Katana Bakung Lelabah Merah
+▫170-200 2H Golok Pembasmi Naga / 1H Pedang Archanida
+▫200-225 Baju Dara / Kostum Pengelabu
+▫225-250 2H Pholidata / Kostum Pengelabu
+▫250-300 Gak Tau`, m)
       return;
     }
   } catch (err) {
