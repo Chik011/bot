59db3f6293783de181ffffc929440353e989b105
Chik011 <chikokasa11@gmail.com>
2025-05-31 14:40:14 +0700

    1

--- plugins/toram.js
+++ plugins/toram.js
@@ -2,17 +2,20 @@ let handler = async (m, { conn, command }) => {
   await conn.reply(m.chat, wait, m)
   try {
     if (command == 'buff') {
-      'buff code belum tersedia';
+      throw 'buff code belum tersedia';
     }
     if (command == 'lvlg') {
-        'lvlg code belum tersedia';
+      throw 'lvlg code belum tersedia';
+    }
+    if (command == 'pembolong') {
+      throw 'belum tersedia';
     }
    } catch (err) {
   console.error(err)
   throw "🚩 Terjadi kesalahan"
    };
 };
-handler.command = handler.help = ['buff','lvlg',]
+handler.command = handler.help = ['buff','lvlg','pembolong'];
 handler.tags = ['toram']
 handler.limit = false;
 handler.premium = false;
