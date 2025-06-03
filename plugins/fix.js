bceaf6f9c6945e476454ce7b282bfe4d863b7e19
Chik011 <chikokasa11@gmail.com>
2025-06-03 22:52:31 +0700

    4

--- plugins/fix.js
+++ plugins/fix.js
@@ -1,22 +1,6 @@
 module.exports = {
   command: ['fix'],
-  tags: ['tools'],
-  help: ['fix'],
-  group: true,
-  admin: true,
   handler: async function (m) {
-    console.log('📥 Fix command dipanggil oleh:', m.sender)
-
-    this.msgqueque = this.msgqueque || []
-    this.queue = this.queue || {}
-
-    const chat = m.chat
-    const before = this.msgqueque.length
-    this.msgqueque = this.msgqueque.filter(id => !id.includes(chat))
-    if (this.queue[chat]) delete this.queue[chat]
-
-    await this.sendMessage(m.chat, {
-      text: `✅ Sesi diperbaiki.\nDihapus dari queue: ${before - this.msgqueque.length}`
-    }, { quoted: m })
+    await m.reply('✅ Ini respons dari command .fix')
   }
 }
