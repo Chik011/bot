fed5e363d892a79357c2bd4a751e2c0532a6a3fa
Chik011 <chikokasa11@gmail.com>
2025-06-07 17:38:01 +0700

    8

--- plugins/laurens.js
+++ plugins/laurens.js
@@ -1,34 +1,46 @@
-const laurensHandler = async (m, { conn }) => {
-  // Check if message contains 'laurens' (case insensitive) and not from status
-  if (/laurens/i.test(m.text) && !m.isStatus) {
-    const responses = [
-      "Ya, ada apa?",
-      "Aku di sini, ada yang bisa dibantu?",
-      "Laurens hadir!",
-      "Memanggilku?",
-      "Aku mendengarmu, katakan saja...",
-      "Hai! Ada yang bisa Laurens bantu?",
-      "Bot Laurens siap membantu!",
-      "Kamu memanggil?",
-      "Laurens online!",
-      "Butuh bantuan apa?"
-    ];
-    
-    const randomResponse = responses[Math.floor(Math.random() * responses.length)];
-    
-    // Send reply with mention
-    await conn.sendMessage(m.chat, {
-      text: randomResponse,
-      mentions: [m.sender]
-    }, { quoted: m });
-  }
+const { 
+    BufferJSON, 
+    WA_DEFAULT_EPHEMERAL, 
+    generateWAMessageFromContent, 
+    proto, 
+    generateWAMessageContent, 
+    generateWAMessage, 
+    prepareWAMessageMedia, 
+    areJidsSameUser, 
+    getContentType 
+} = require('@adiwajshing/baileys')
+
+let handler = async (m, { conn }) => {
+    // Check if message contains 'laurens' (case insensitive)
+    if (/laurens/i.test(m.text)) {
+        const responses = [
+            "Ya, ada apa?",
+            "Aku di sini, ada yang bisa dibantu?",
+            "Laurens hadir!",
+            "Memanggilku?",
+            "Aku mendengarmu, katakan saja...",
+            "Hai! Ada yang bisa Laurens bantu?",
+            "Bot Laurens siap membantu!",
+            "Kamu memanggil?",
+            "Laurens online!",
+            "Butuh bantuan apa?"
+        ];
+        
+        const randomResponse = responses[Math.floor(Math.random() * responses.length)];
+        
+        // Send reply with mention
+        await conn.sendMessage(m.chat, {
+            text: randomResponse,
+            mentions: [m.sender]
+        }, { quoted: m });
+    }
 }
 
 // Handler configuration
-laurensHandler.help = ['laurens'];
-laurensHandler.tags = ['general'];
-laurensHandler.command = false; // Disable command mode
-laurensHandler.group = true; // Works in groups
-laurensHandler.private = true; // Works in private chats
+handler.help = ['laurens'];
+handler.tags = ['general'];
+handler.command = /^(laurens|bot)$/i;  // Responds to both 'laurens' and 'bot' commands
+handler.group = true;
+handler.private = true;
 
-module.exports = laurensHandler;
+module.exports = handler;
