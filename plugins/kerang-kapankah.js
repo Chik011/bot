21c06ce7e3582a71adc1608765214668232799c3
Chik011 <chikokasa11@gmail.com>
2025-06-07 17:22:12 +0700

    8

--- plugins/kerang-kapankah.js
+++ plugins/kerang-kapankah.js
@@ -1,29 +1,33 @@
-// let handler = async (m, { conn, text }) => {
-//   conn.reply(m.chat, `Kayaknya ${Math.floor(Math.random() * 100)} ${pickRandom(['detik', 'menit', 'jam', 'hari', 'minggu', 'bulan', 'tahun', 'abad'])} lagi ...
-// `.trim(), m, m.mentionedJid ? {
-//   contextInfo: {
-//     mentionedJid: m.mentionedJid
-//   }
-// } : {})
-// }
-// handler.help = ['', 'kah'].map(v => 'kapan' + v + ' <text>?')
-// handler.tags = ['kerang']
-// handler.customPrefix = /(\?$)/
-// handler.command = /^kapan(kah)?$/i
-// handler.owner = false
-// handler.mods = false
-// handler.premium = false
-// handler.group = false
-// handler.private = false
+let handler = async (m, { conn, text }) => {
+  conn.reply(m.chat, `${Math.floor(Math.random() * 100)} ${pickRandom(["Aku Laurens, bot siap membantu!",
+      "Hai! Ada yang bisa kubantu?",
+      "Laurens di sini, ngomong aja ya.",
+      "Butuh bantuan? Tanyakan ke Laurens!",
+      "Aku bot sederhana, tapi siap menemani kamu."])}
+`.trim(), m, m.mentionedJid ? {
+  contextInfo: {
+    mentionedJid: m.mentionedJid
+  }
+} : {})
+}
+handler.help = [''].map(v => 'laurens')
+handler.tags = ['kerang']
+handler.customPrefix = /(\?$)/
+handler.command = /^laurens?$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
 
-// handler.admin = false
-// handler.botAdmin = false
+handler.admin = false
+handler.botAdmin = false
 
-// handler.fail = null
+handler.fail = null
 
-// module.exports = handler
+module.exports = handler
 
-// function pickRandom(list) {
-//   return list[Math.floor(Math.random() * list.length)]
-// }
+function pickRandom(list) {
+  return list[Math.floor(Math.random() * list.length)]
+}
 
