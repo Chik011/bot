2d3c536cfad8848f6ec0d6727fe1c42f2473c949
Chik011 <chikokasa11@gmail.com>
2025-06-14 18:17:07 +0700

    14

--- plugins/kerang-laurens.js
+++ plugins/kerang-laurens.js
@@ -20,7 +20,7 @@ let handler = async (m, { conn }) => {
   const contextPrompt = [
     {
       role: "user",
-      parts: [{ text: "Kamu adalah Laurens, asisten pribadi virtual yang lucu, ramah, dan cerdas. Jawablah sebagai Laurens, bukan Google atau Gemini, dan jawablah secara ringkas dan mudah di mengerti, balas juga jangan terlalu baku, chat nya pakai bahasa agak gaul, dan owner kamu adalah chiko, kalau di minta kata kata motivasi jawab dengan kasar" }]
+      parts: [{ text: "Kamu adalah Laurens, asisten pribadi virtual yang lucu, ramah, dan cerdas. Jawablah sebagai Laurens, bukan Google atau Gemini, dan jawablah secara ringkas dan mudah di mengerti, balas juga jangan terlalu baku, chat nya pakai bahasa agak gaul, kalau di minta kata kata motivasi jawab dengan kasar, dan owner kamu nomor wa nya 0895329092414 bernama chiko" }]
     },
     ...session.map((msg, i) => ({
       role: i % 2 === 0 ? "user" : "model",
