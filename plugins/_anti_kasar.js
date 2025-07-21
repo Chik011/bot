d01f914892eafecf26f49fd4fbf8d46215962200
Chik011 <chikokasa11@gmail.com>
2025-07-21 22:26:28 +0700

    23

--- plugins/_anti_kasar.js
+++ plugins/_anti_kasar.js
@@ -1,8 +1,12 @@
 let handler = async (m, { conn }) => {
+    // Hapus pesannya
     await conn.sendMessage(m.chat, { delete: m.key });
 };
 
-handler.customPrefix = /anjing|babi|kontol|memek|peler|ngentot|bangsat|tolol|idiot|goblok|kampang|kimak|jembut|pepek|asu|bitch|fuck|shit|tai|lonte|bencong/i;
+// Pakai \b supaya hanya kata berdiri sendiri yang kena (tidak potong kata lain)
+handler.customPrefix = /\b(anjing|babi|kontol|memek|peler|ngentot|bangsat|tolol|idiot|goblok|kampang|kimak|jembut|pepek|asu|bitch|fuck|shit|tai|lonte|bencong)\b/i;
+
+// handler.command pakai regex kosong karena ini trigger dari customPrefix, bukan perintah
 handler.command = new RegExp();
 
 module.exports = handler;
---
+++ plugins/toram-upxtall.js
@@ -0,0 +1,149 @@
+const xtallData = {
+  senjata: `🔴 *Crysta Senjata:*
+Pedang Sihir Iblis ➔ Kristal Berlumur Darah
+Dukun Lapin ➔ Momok Gelembung
+Golem Galian ➔ Builder Golem
+Zahhak Machina ➔ Penyihir Besi Bercakar
+Imitacia ➔ Finstern si Naga Kegelapan ➔ Oculasignio
+Armasit ➔ Florizard
+Ganglef ➔ Kepiting Bulan Besar
+Ganglef ➔ Machina Tiran ➔ Vulture ➔ Naga Penyamar Mimyugon
+Ksatria Buruk Dusta ➔ Gwaimol ➔ Hexter ➔ Don Profundo
+Potum Pahlawan ➔ Potum Pahlawan II ➔ Potum Pahlawan III
+Iblis Kristal Jahanam ➔ Shampy ➔ Irestida
+Imitator ➔ Mardula ➔ Velum
+Sang Juara Megiston ➔ Sang Juara Megiston II ➔ Sang Juara Megiston III ➔ Sang Juara Megiston IV ➔ Sang Juara Megiston V ➔ Sang Juara Megiston VI
+Gerbang Iblis ➔ Mozto Machina ➔ Pisteus ➔ Naga Jahat Fazzino
+Pedang Neo Maton ➔ Pedrio
+Zolban ➔ Rephton ➔ Naga Trompet Reguita
+Golem Pilar ➔ Machina Ultima ➔ Vlam si Naga Api
+Mbah Dukun Usasama ➔ Mbah Dukun Usasama II
+Zega ➔ Zega II ➔ Zega III ➔ Zega IV ➔ Zega V ➔ Zega VI ➔ Zega VII
+Pomie Chan ➔ Pomie Chan II
+Metasrigala ➔ Cakar Kucing Kissatpam`,
+
+  zirah: `🟡 *Crysta Zirah:*
+Tortuga ➔ Daddy Finpen ➔ Capo Profundo
+Quasar Jahanam ➔ Mata Jahanam
+Kruztor ➔ Kruztor II
+Cerabes ➔ Mimesia ➔ Sakura Merah Jelita ➔ Baavgai
+Boss Roga ➔ Iconos ➔ Ornlarf ➔ Roga Safir ➔ Ferzen si Naga Batu ➔ Walican
+Pilz Erosi ➔ Filrocas
+Golem Preman ➔ Naga Langkisau
+Cerberus ➔ Pyxtica ➔ Gemma
+Forestia ➔ Glaucius
+Colon Komandan ➔ Goleps
+Ifrid ➔ Mama Fluck ➔ Gordel
+Noeliel ➔ Noeliel si Patung Es Suci ➔ Kucing Yule
+Elang Zamrud ➔ Jamur Super Mampus
+Usakichi ➔ Usami ➔ Usamochi
+Gopherga ➔ Yuveria
+Sosok Jahat ➔ Velly Hitam
+DX Fighter ➔ DX Fighter II
+Dr. Leonardo ➔ Dr. Leonardo II
+Arachnidemon ➔ Zapo`,
+
+  perkakas: `🔵 *Crysta Perkakas Tambahan:*
+Ayah Yashiro Azuki ➔ ✝Raja Kegelapan✝
+Biskuit Buatan Tangan ➔ Alfenix
+Ratu Kuno ➔ Ratu Kuno II
+Naga Senja ➔ Baphomela ➔ Bayangan Biru Tafakur
+Warmonger ➔ Proto Leon ➔ Raja Piton ➔ Naga Membara Igneus
+Candela ➔ Candela II
+Chocolate Ooze ➔ Chocolate Ooze II ➔ Amargon
+Kapten Karatan ➔ Exdocus
+Iconos Emas ➔ Felicitoad ➔ Goldigem
+Raja Kerbau ➔ Pillow Kitagawa
+Raja Kerbau ➔ Paduka Raja Kerbau
+Eidenliebe ➔ Garnache
+Pumpking ➔ Jeila ➔ Zoe ➔ Zarth ➔ Neewollah
+Celeng Raksasa ➔ Mega Alpoca
+Naga Sabana Yelb ➔ Roda Kelana ➔ Neo Roda Kelana
+Gespenst II ➔ Stellar Ooze
+Tengkorak Emas ➔ Solopy
+Seltirio ➔ Tardigrademon
+Dusk Machina ➔ Trokostida
+Adaro ➔ Monster Bawah Laut
+Naga Sabana Yelb ➔ Roda Kelana ➔ Gordo
+Narumi Hina ➔ Ibu Yashiro Azuki II
+Monster dalam game`,
+
+  spesial: `🟣 *Crysta Perkakas Spesial:*
+Viscum ➔ Deniala ➔ Amalgam ➔ Crysmort
+Venena ➔ Venena II
+Baron Bling-Bling ➔ Roh Orang Mati
+Ooze ➔ Lalvada ➔ Dominaredor
+Stone Mercenary ➔ Memecoleous
+Eerie Crystal ➔ Kristal Misterius
+Kristal Jahat ➔ Tapir ➔ Patissia
+Violaccoon ➔ Ketua Bandit Gurun
+Dark Mushroom ➔ Teertocrit
+Bexiz ➔ Zelbuse ➔ Naga Milisi Turba
+Volgagon ➔ Etoise`,
+
+  normal: `⚪ *Crysta Normal:*
+Metal Stinger ➔ Kapten Lyark Spesialis ➔ Ageladanios
+Crimsoch ➔ Amoeba Machina
+Flare Volg ➔ Charugon
+Naga Beringas Decel ➔ York ➔ Tuscog ➔ Bayangan Hitam
+Aranea ➔ Blazingur
+Ksatriaja ➔ Volotur ➔ Brassozard
+Gespenst ➔ Salamander ➔ Bullamius
+Shawle ➔ Dutannen
+Nurethoth ➔ Guignol ➔ Golem Satpam
+Radibat ➔ Melancia
+Nurethoth ➔ Nuthoreth
+Raja Potum ➔ Potum Platinum
+Minotaur ➔ Rhinosaurus
+Coryn Besar ➔ Seraph Machina
+Odelon Machina ➔ Pret
+Bos Petapa Kadal ➔ Orictoceras
+Gravicep ➔ Naga Abu Merah Rudis`
+};
+
+// Handler .xtall [kategori atau keyword]
+let handler = async (m, { conn, text, command }) => {
+  if (command === 'allxtall') {
+    let all = Object.values(xtallData).join('\n\n');
+    return conn.reply(m.chat, all, m);
+  }
+
+  if (command === 'xtallrandom') {
+    let allLines = Object.values(xtallData).join('\n').split('\n').filter(line => line.includes('➔'));
+    let pick = allLines[Math.floor(Math.random() * allLines.length)];
+    return conn.reply(m.chat, `🎲 *Random Crysta:*\n${pick}`, m);
+  }
+
+  let keyword = text ? text.toLowerCase() : '';
+  if (!keyword) {
+    return conn.reply(m.chat, `🔍 Gunakan:\n.xtall senjata\n.xtall zirah\n.xtall perkakas\n.xtall spesial\n.xtall normal\n.xtall [nama bebas]`, m);
+  }
+
+  if (xtallData[keyword]) {
+    return conn.reply(m.chat, xtallData[keyword], m);
+  }
+
+  let found = [];
+  for (let kategori in xtallData) {
+    let lines = xtallData[kategori].split('\n').filter(line => line.toLowerCase().includes(keyword));
+    if (lines.length > 0) {
+      found.push(xtallData[kategori].split('\n')[0]);
+      found.push(...lines);
+      found.push('');
+    }
+  }
+
+  if (found.length === 0) {
+    return conn.reply(m.chat, `❌ Tidak ditemukan: *${text}*`, m);
+  }
+
+  return conn.reply(m.chat, `🔍 *Hasil Pencarian "${text}":*\n\n${found.join('\n')}`, m);
+};
+
+handler.command = ['xtall', 'allxtall', 'xtallrandom'];
+handler.tags = ['toram'];
+handler.help = ['xtall [kategori / keyword]', 'allxtall', 'xtallrandom'];
+handler.limit = false;
+handler.premium = false;
+
+module.exports = handler;
